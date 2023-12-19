c2fd6c0808e4c8c62a8c92949b5cd3bf8fb3893a
Lawson Abraham <abraham@MBP-de-Lawson.localdomain>
2023-12-19 16:41:41 +0100

    Insertion des données dans la database, recuperation des données avec la methode all et filtration avec la methode where

--- app/Http/Controllers/GetVoicesController.php
+++ app/Http/Controllers/GetVoicesController.php
@@ -2,45 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Casts\Json;
+use App\Models\Voice;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class GetVoicesController extends Controller
 {
     public function index():JsonResponse{
-        $voices = [
-            'id' => 1,
-            'language' => "fr-FR",
-            'name' => "Denise",
-            'gender' => "Female",
-            'created_at' => "2023-01-01 08:08:00",
-            'updated_at' => "2023-01-02 08:08:00",
-            [
-                'id' => 2,
-                'language' => "en-GB",
-                'name' => "Ryan",
-                'gender' => "Male",
-                'created_at' => "2023-01-01 08:08:00",
-                'updated_at' => "2023-01-02 08:08:00",
-            ],
-            [
-                'id' => 3,
-                'language' => "en-GB",
-                'name' => "Elodie",
-                'gender' => "Female",
-                'created_at' => "2023-01-01 08:08:00",
-                'updated_at' => "2023-01-02 08:08:00",
-            ],
-            [
-                'id' => 4,
-                'language' => "en-GB",
-                'name' => "Jules",
-                'gender' => "Male",
-                'created_at' => "2023-01-01 08:08:00",
-                'updated_at' => "2023-01-02 08:08:00",
-            ]
-        ];
-        return new JsonResponse($voices,200);
+
+        //Conditions pour filtrer la requete.
+        return new JsonResponse( Voice::get(),200);
+
     }
 }
