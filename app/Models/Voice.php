cdecc9c32fe257eeea8b5f7d8d15b4a55ee71a2f
Lawson Abraham <abraham@MacBook-Pro-de-Lawson.local>
2023-12-20 00:58:49 +0100

    Add new endpoint api voices/locale for display the last five votes in descending order

--- app/Models/Voice.php
+++ app/Models/Voice.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Voice extends Model
 {
     use HasFactory;
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('voices',[GetVoicesController::class, "index"]);
+Route::get('voices/locale',[GetVoicesController::class,"locale"]);
 
