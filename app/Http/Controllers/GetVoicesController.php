021e6194f35cf3803c71b581e23fa18c77938d1f
Lawson Abraham <abraham@MBP-de-Lawson.localdomain>
2023-12-20 15:32:02 +0100

    add-new-endpoint POST /voices for create new Voices with params locale, gender, name

--- app/Http/Controllers/GetVoicesController.php
+++ app/Http/Controllers/GetVoicesController.php
@@ -13,4 +13,14 @@ class GetVoicesController extends Controller
         return new JsonResponse( Voice::get(),200);
 
     }
+    public function insertionVoices(Request $request):JsonResponse{
+
+        $voice = new Voice();
+        $voice->locale= $request->get('locale');
+        $voice->gender= $request->get('gender');
+        $voice->name= $request->get('name');
+        $voice->save();
+
+        return new JsonResponse(Voice::orderBy('created_at','desc')->limit(1)->get(),201);
+    }
 }
