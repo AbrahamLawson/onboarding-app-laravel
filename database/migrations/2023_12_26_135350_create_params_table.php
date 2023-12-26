5169ab76991da984afbe28ad197e4b575821d0b0
Lawson Abraham <abraham@MacBook-Pro-de-Lawson.local>
2023-12-26 15:10:45 +0100

    migration entities params

---
+++ database/migrations/2023_12_26_135350_create_params_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('params', function (Blueprint $table) {
+            $table->id();
+			$table->foreign('project_id')->references('id')->on('projects');
+			$table->text('value', 100);
+			$table->unsignedInteger('order');
+			$table->bigInteger('project_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('params');
+    }
+};
